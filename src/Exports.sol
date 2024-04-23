ca47ac684edde5b44ad0e240a712c1a195ca8165
Kevin Upton <k_upton@live.com>
2024-04-23 23:16:54 +1000

    v0.0.2

--- src/Exports.sol
+++ src/Exports.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/interfaces/IERC20Metadata.sol";
 import "@openzeppelin/contracts/interfaces/IERC721.sol";
 import "@openzeppelin/contracts/utils/Multicall.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
